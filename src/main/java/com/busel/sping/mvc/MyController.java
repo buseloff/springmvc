4bd8a4c01f356bbf51322f78f6c2e345dc3fcd5a
Busel Oleksandr <buseloff@gmail.com>
2022-07-14 17:18:48 +0300

    Added validation

---
+++ src/main/java/com/busel/sping/mvc/MyController.java
@@ -0,0 +1,59 @@
+package com.busel.sping.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+
+@Controller
+@RequestMapping("/employee")
+public class MyController {
+    @RequestMapping("/")
+    public String showFirstView() {
+        return "first-view";
+    }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails(Model model) {
+//        Employee emp = new Employee();
+//        emp.setName("Ivan");
+//        emp.setSurname("Petrov");
+//        emp.setSalary(500);
+        model.addAttribute("employee", new Employee());
+        return "ask-emp-details-view";
+    }
+
+    //    @RequestMapping("showDetails")
+//    public String showEmpDetails() {
+//        return "show-emp-details-view";
+//    }
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model) {
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//        model.addAttribute("description", " - udemy instructor");
+//        return "show-emp-details-view";
+//    }
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp
+            , BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "ask-emp-details-view";
+        } else {
+            return "show-emp-details-view";
+        }
+//        String name = emp.getName();
+//        emp.setName("Mr. " + name);
+//        String surname = emp.getSurname();
+//        emp.setSurname(surname + "!");
+//        int salary = emp.getSalary();
+//        emp.setSalary(salary * 10);
+
+
+    }
+}
